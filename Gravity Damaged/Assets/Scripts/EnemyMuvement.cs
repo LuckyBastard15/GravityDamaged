a3a7ce5cb02afed450ea7a1619ec54676b01047d
LuckyBastard15 <85241539+LuckyBastard15@users.noreply.github.com>
2022-04-04 07:21:11 -0500

    fixed

--- Gravity Damaged/Assets/Scripts/EnemyMuvement.cs
+++ Gravity Damaged/Assets/Scripts/EnemyMuvement.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class EnemyMuvement : MonoBehaviour
 {
+    [SerializeField] private float _speed = 10;
     //[SerializeField] private float _speed = 13;
     void Update()
     {
-        //transform.Translate(Vector3.down *_speed *Time.deltaTime);
+        transform.Translate(Vector3.down *_speed *Time.deltaTime);
     }
 }
